bcfe9b82a319bf8bc6134647c1896529b75bfa44
Семенов Илья <i.semenov@alfa2b.ru>
2022-12-24 11:17:12 +0300

    migration init

---
+++ m/20221224111632_init.py
@@ -0,0 +1,14 @@
+"""
+This module contains a Caribou migration.
+
+Migration Name: init 
+Migration Version: 20221224111632
+"""
+
+def upgrade(connection):
+    # add your upgrade step here
+    pass
+
+def downgrade(connection):
+    # add your downgrade step here
+    pass
