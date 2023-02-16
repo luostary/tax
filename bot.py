06c88a458ee4b1ddfb520b769c5063e07fed7fcc
Семенов Илья <i.semenov@alfa2b.ru>
2023-02-16 22:12:51 +0300

    infinite polling

--- bot.py
+++ bot.py
@@ -8,4 +8,10 @@ from db import BotDB
 BotDB = BotDB('taxi.db')
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+#    executor.start_polling(dp, skip_updates=True)
+    while True:
+        try:
+            executor.start_polling(dp, skip_updates=True)
+        except Exception as e:
+            time.sleep(2)
+            print(e)
