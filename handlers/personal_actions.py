2eb6dc520ef0c86afb1f917a80b7821049ce9d93
Семенов Илья <i.semenov@alfa2b.ru>
2023-03-04 01:10:56 +0300

    input name fix

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -143,10 +143,8 @@ async def inlineClick(message, state: FSMContext):
         print(message.from_user.id)
         await setName(message)
     elif message.data == 'clientNameSaved':
-        await state.finish()
         await setPhone(message)
     elif message.data == 'clientPhoneSaved':
-        await state.finish()
         await setDeparture(message, state)
     elif message.data == "driver":
         if(not BotDB.driver_exists(message.from_user.id)):
