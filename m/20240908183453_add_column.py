b5c8b14212f92b60835c63bdeaed2332dac91fa5
Семенов Илья <i.semenov@alfa2b.ru>
2024-09-09 18:50:13 +0300

    Миграция с новым полем driver

---
+++ m/20240908183453_add_column.py
@@ -0,0 +1,15 @@
+"""
+This module contains a Caribou migration.
+
+Migration Name: add_column 
+Migration Version: 20240908183453
+"""
+
+def upgrade(connection):
+    sql = "ALTER TABLE `driver` ADD COLUMN `user_type` VARCHAR(45) NULL COMMENT 'Тип пользователя' AFTER `referer_payed`;";
+    sql = "UPDATE driver set user_type = 'driver' where user_type is null limit 1000;"
+    pass
+
+def downgrade(connection):
+    # add your downgrade step here
+    pass
