41824f32407dafa8df365d74b1aec852304ebd85
Семенов Илья <i.semenov@alfa2b.ru>
2024-09-23 21:11:05 +0300

    Новое поле подписки

--- m/20240908183453_add_column.py
+++ m/20240908183453_add_column.py
@@ -8,6 +8,7 @@ Migration Version: 20240908183453
 def upgrade(connection):
     sql = "ALTER TABLE `driver` ADD COLUMN `user_type` VARCHAR(45) NULL COMMENT 'Тип пользователя' AFTER `referer_payed`;";
     sql = "UPDATE driver set user_type = 'driver' where user_type is null limit 1000;"
+    sql = "ALTER TABLE `driver` ADD COLUMN `dt_subscribe_until` DATETIME NULL AFTER `user_type`"
     pass
 
 def downgrade(connection):
